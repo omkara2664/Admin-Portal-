10202969dcf62d52c8cbb47bd687129db9d1bffc
Omkar Patil <omkara2664@gmail.com>
2022-11-17 16:58:20 +0530

    MERN Fullstack Project Basic file-folder structure

---
+++ backend/app.js
@@ -0,0 +1,2 @@
+console.log("Hello Omkar");
+console.log('Let\'s Build the Project');
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Namaskar Omkar</h1>
+      let's Build the World !!
+    </div>
+  );
+}
+
+export default App;
